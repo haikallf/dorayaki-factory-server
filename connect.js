d50b95aab96b1804df7da7fc5f73f4577bec0acc
haikallf <haikallzrd@gmail.com>
2021-11-19 14:40:11 +0700

    connect to db mysql

---
+++ connect.js
@@ -0,0 +1,20 @@
+var mysql = require("mysql");
+
+// connect to db
+
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "haikallf17",
+  database: "pabrik",
+});
+
+conn.connect((err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log("Connected to pabrik");
+  }
+});
+
+module.exports = conn;
