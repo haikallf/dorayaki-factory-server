c9ea940d9895b8e1b92a802cb3729d429ebc7245
haikallf <haikallzrd@gmail.com>
2021-11-19 22:24:52 +0700

    added middleware for auth (registration)

---
+++ middleware/auth.js
@@ -0,0 +1,43 @@
+var connection = require("../connect");
+var mysql = require("mysql");
+var md5 = require("MD5");
+var response = require("../res");
+var jwt = require("jsonwebtoken");
+var config = require("../config/secret");
+var ip = require("ip");
+
+// controller for registration
+exports.registration = function (req, res) {
+  var post = {
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    name: req.body.name,
+  };
+
+  var query = "SELECT email FROM ?? WHERE ??";
+  var table = ["user", "email", post.email];
+
+  query = mysql.format(query, table);
+
+  connection.query(query, function (error, rows) {
+    if (error) {
+      console.log(error);
+    } else {
+      if (rows.length == 0) {
+        var query = "INSERT INTO ?? SET ?";
+        var table = ["user"];
+        query = mysql.format(query, table);
+        connection.query(query, post, function (error, rows) {
+          if (error) {
+            console.log(error);
+          } else {
+            response.ok("Registration success!", res);
+          }
+        });
+      } else {
+        response.ok("Email is already used!");
+      }
+    }
+  });
+};
