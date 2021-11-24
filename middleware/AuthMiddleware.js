242dd01800a0c7bf96b71aa7199d327646947519
haikallf <haikallzrd@gmail.com>
2021-11-24 23:07:33 +0700

    new middleware

--- middleware/AuthMiddleware.js
+++ middleware/AuthMiddleware.js
@@ -1,18 +1,21 @@
+const e = require("express");
 const { verify } = require("jsonwebtoken");
 
 const validateToken = (req, res, next) => {
-  const accessToken = req.header["accessToken"];
+  console.log(req.headers["Authorization"]);
+  const accessToken = req.headers["Authorization"];
 
   if (!accessToken) {
     res.json({ error: "User not logged in!" });
-  }
-  try {
-    const validToken = verify(accessToken, "jwtsecret");
-    if (validToken) {
-      return next();
+  } else {
+    try {
+      const validToken = verify(accessToken, "jwtsecret");
+      if (validToken) {
+        return next();
+      }
+    } catch (err) {
+      return res.json({ error: err });
     }
-  } catch (err) {
-    return res.json({ error: err });
   }
 };
 
